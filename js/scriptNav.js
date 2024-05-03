e26d66587ae52ba260ddc2bb8ac6aab9e0a35f26
deep-dot <59271651+deep-dot@users.noreply.github.com>
2024-05-03 23:08:20 +1000

    home page

--- js/scriptNav.js
+++ js/scriptNav.js
@@ -13,6 +13,7 @@
         var more = document.querySelector('.more > a');
         var navbar = document.querySelector('.navbar');
         let isDropdownOpen = true; // Track dropdown open state
+        navbar.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 0%, rgba(255,224,158,1) 50%)';
     
         more.addEventListener('click', function(event) {
             event.preventDefault(); // Prevent default link behavior
@@ -21,11 +22,12 @@
             if (isDropdownOpen && dropdown.style.display === 'flex') {
                 dropdown.style.display = 'none';
                 isDropdownOpen = false;
+                // https://cssgradient.io/
                 navbar.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 0%, rgba(255,224,158,1) 50%)';
             } else {
                 dropdown.style.display = 'flex';
                 isDropdownOpen = true;
-                navbar.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 100%, rgba(255,224,158,1) 0%)';
+                navbar.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 100%, rgba(255,224,158,1) 15%)';
             }
             //dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex'; // Toggle display
         });
