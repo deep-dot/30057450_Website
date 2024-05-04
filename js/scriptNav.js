28f08a933b75f6479bca7813728a0ac136ecc20f
deep-dot <59271651+deep-dot@users.noreply.github.com>
2024-05-04 15:37:30 +1000

    travell page

--- js/scriptNav.js
+++ js/scriptNav.js
@@ -4,7 +4,9 @@
         var more = document.querySelector('.more > a');
         var dropdown = document.querySelector('.more .dropdown-content');
         var navbar = document.querySelector('.navbar');
+        var navsection = document.querySelector('.navsection');
         var bodyContent = document.querySelector('.body-content');
+        navsection.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 0%, rgba(255,224,158,1) 50%)';
         navbar.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 0%, rgba(255,224,158,1) 50%)';
         var height
         dropdown.style.display = 'none';
@@ -12,6 +14,7 @@
         function closeDropdown() {
             dropdown.style.display = 'none';
             navbar.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 0%, rgba(255,224,158,1) 50%)';
+            navsection.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 0%, rgba(255,224,158,1) 50%)';
             bodyContent.style.paddingTop = 0;
         }
     
@@ -24,6 +27,7 @@
             } else {
                 dropdown.style.display = 'flex';
                 navbar.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 100%, rgba(255,224,158,1) 15%)';
+                navsection.style.background = 'linear-gradient(180deg, rgba(153,134,95,1) 100%, rgba(255,224,158,1) 15%)';
                 const height = navbar.offsetHeight; 
                 bodyContent.style.paddingTop = `${height}px`;
             }
