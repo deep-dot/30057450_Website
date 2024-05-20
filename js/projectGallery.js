143e605ea30950c75b75a20987876bad6087bb4d
deep-dot <59271651+deep-dot@users.noreply.github.com>
2024-05-20 23:36:35 +1000

    .

--- js/projectGallery.js
+++ js/projectGallery.js
@@ -1,31 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     var currentIndex = 0;
-//     var images = document.querySelectorAll('.carousel-item');
-//     var prevButton = document.querySelector('.prev');
-//     var nextButton = document.querySelector('.next');
-
-//     function showImage(index) {
-//         images.forEach((item, i) => {
-//             if (i === index) {
-//                 item.classList.add('active');
-//             } else {
-//                 item.classList.remove('active');
-//             }
-//         });
-//     }
-
-//     prevButton.addEventListener('click', function () {
-//         currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
-//         showImage(currentIndex);
-//     });
-
-//     nextButton.addEventListener('click', function () {
-//         currentIndex = (currentIndex + 1) % images.length;
-//         showImage(currentIndex);
-//     });
-
-//     showImage(currentIndex);
-// });
 
 
 const buttons = document.querySelectorAll("[data-carousel-button]")
